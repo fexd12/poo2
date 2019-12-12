dbd6e39c7ab844507f88f2d9777dbcd3d08f764f
fexd12 <felipe.demelo@hotmail.com>
2019-12-12 18:38:25 -0300

    commit

---
+++ poo-project-api/POOProject/Controllers/SistemaCadastroController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using POOProject.Models;
+using POOProject.Services;
+
+namespace POOProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SistemaCadastroController : ControllerBase
+    {
+        private static ISistemaCadastroService _sistemaCadastroService; 
+
+        public SistemaCadastroController(ISistemaCadastroService sistemaCadastroService)
+        {
+            _sistemaCadastroService = sistemaCadastroService;
+        }
+
+
+        [HttpGet]
+        [Route("ListarSmartphones")]
+        public List<Smartphone> ListarSmartphones()
+        {
+            return _sistemaCadastroService.ListarSmartphones();
+        }
+
+        [HttpPost]
+        [Route("CadastrarSmartphones")]
+        public List<Smartphone> CadastrarSmartphones([FromBody]Smartphone smartphone)
+        {
+            return _sistemaCadastroService.CadastrarSmartphones(smartphone);
+        }
+    }
+}
---
+++ poo-project-api/POOProject/Models/Smartphone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POOProject.Models
+{
+    public class Smartphone
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Problema { get; set; }
+
+    }
+}
